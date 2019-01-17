72dbfd50e80e6589e0b69eee57555d9ef1ffdbc7
David <ntd.david@protonmail.com>
2019-01-17 15:36:54 -0800

    moved file location to subfolder. Changed default baud rate

---
+++ ds_i2cScan/ds_i2cScan.ino
@@ -0,0 +1,44 @@
+/*
+ * Demonstrate use of function to find devices on I2C port
+ */
+
+#include <Arduino.h>
+#include <Wire.h>         // i2c library
+
+void setup() {
+    serialInit(115200);   // set up usb serial port
+
+    i2cScan();        // requires serial port to be initialized prior to use
+}
+
+
+void loop(void) {
+    ;
+}
+
+
+void i2cScan() {
+    Serial.println ("\ni2cScan...\n");
+    static int count = 0;
+
+    Wire.begin();
+    for (int i = 8; i < 120; i++)
+    {
+        Wire.beginTransmission(i);          // probe this i2c address
+        if (Wire.endTransmission() == 0)
+        {
+            Serial.print ("  device at: 0x" + String(i, HEX) + "  [" + String(i) + "] \n");
+            count++;
+            delay (1);
+        }
+    }
+    Serial.print("\nTotal devices:  " + String(count) + "\n");
+}
+
+
+// Serial initialization accounts for integrated USB, as on Teensy3 and Due and some others
+void serialInit(int baudrate) {
+    Serial.begin(baudrate); // serial IO routed through USB console
+    while (!Serial && (millis() < 5000)) {} // include timeout if print console isn't opened
+    Serial.println("USB Serial Print initialized.");
+}
